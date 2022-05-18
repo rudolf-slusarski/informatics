680252720f6974ea67b39c26fb29031521a0199f
rudolf <rudolf.slusarski@gmail.com>
2022-05-18 03:13:32 +0200

    data types in quicksort are seriously wrong

--- src/algorithms/quicksort.rs
+++ src/algorithms/quicksort.rs
@@ -7,7 +7,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-fn quicksort(arr: &mut Vec<usize>, p: usize, r: usize) -> Vec<usize> {
+fn quicksort(arr: &mut Vec<isize>, p: isize, r: isize) -> Vec<isize> {
     if p < r {
         let q = partition(arr, p, r);
         quicksort(arr, p, q);
@@ -16,13 +16,13 @@ fn quicksort(arr: &mut Vec<usize>, p: usize, r: usize) -> Vec<usize> {
     arr.clone()
 }
 
-fn partition(arr: &mut Vec<usize>, p: usize, r: usize) -> usize {
-    let x = arr[r];
-    let mut i = p - 1;
+fn partition(arr: &mut Vec<isize>, p: isize, r: isize) -> isize {
+    let x = arr[r as usize];
+    let mut i = p as isize - 1;
     for (_, j) in (p..=r).enumerate() {
-        if arr[j] <= x {
+        if arr[j as usize] <= x {
             i += 1;
-            arr.swap(i, j);
+            arr.swap(i as usize, j as usize);
         }
     }
     if i < r {
@@ -35,7 +35,7 @@ fn partition(arr: &mut Vec<usize>, p: usize, r: usize) -> usize {
 pub fn run() -> Result<(), Error> {
     let total_time = Instant::now();
     let range = Uniform::from(0..=1_000_000);
-    let descending: Vec<usize> = (1..=100_000).rev().collect();
+    let descending: Vec<isize> = (1..=100_000).rev().collect();
 
     println!(
         "{:^7} | {:^12} | {:^12}",
@@ -44,24 +44,24 @@ pub fn run() -> Result<(), Error> {
     println!("--------------------------------------");
 
     for i in (10_000..=100_000).step_by(5000) {
-        let mut random_numbers: Vec<usize> =
+        let mut random_numbers: Vec<isize> =
             rand::thread_rng().sample_iter(&range).take(i).collect();
-        let mut worst_case: Vec<usize> = descending.clone().into_iter().take(i).collect();
+        let mut worst_case: Vec<isize> = descending.clone().into_iter().take(i).collect();
         let mut results: Vec<Duration> = vec![];
 
         let time = Instant::now();
-        quicksort(&mut random_numbers, 0, i - 1);
+        quicksort(&mut random_numbers, 0, i as isize - 1);
         results.push(time.elapsed());
 
         let time = Instant::now();
-        quicksort(&mut worst_case, 0, i - 1);
+        quicksort(&mut worst_case, 0, i as isize - 1);
         results.push(time.elapsed());
 
         println!(
             "{:^7} | {:>12} | {:>12}",
             i,
-            format!("{:?}", results[0]),
-            format!("{:?}", results[1]),
+            format_args!("{:?}", results[0]),
+            format_args!("{:?}", results[1]),
         );
     }
 
@@ -77,14 +77,14 @@ mod tests {
     #[test]
     fn quicksort_random_250() {
         let range = Uniform::from(0..=1_000);
-        let mut random_250: Vec<usize> = rand::thread_rng().sample_iter(&range).take(250).collect();
+        let mut random_250: Vec<isize> = rand::thread_rng().sample_iter(&range).take(250).collect();
         assert!(quicksort(&mut random_250, 0, 249).is_sorted());
     }
 
     #[test]
     fn quicksort_random_250_000() {
         let range = Uniform::from(0..=1_000_000);
-        let mut random_250_000: Vec<usize> = rand::thread_rng()
+        let mut random_250_000: Vec<isize> = rand::thread_rng()
             .sample_iter(&range)
             .take(250_000)
             .collect();
@@ -93,28 +93,28 @@ mod tests {
 
     #[test]
     fn quicksort_empty() {
-        let mut arr: Vec<usize> = vec![];
-        let sorted: Vec<usize> = vec![];
+        let mut arr: Vec<isize> = vec![];
+        let sorted: Vec<isize> = vec![];
         assert_eq!(quicksort(&mut arr, 0, 0), sorted);
     }
 
     #[test]
     fn quicksort_all() {
-        let mut arr: Vec<usize> = vec![3, 546, 32, 11, 6546, 65, 86, 11, 14, 12];
-        let sorted: Vec<usize> = vec![3, 11, 11, 12, 14, 32, 65, 86, 546, 6546];
+        let mut arr: Vec<isize> = vec![3, 546, 32, 11, 6546, 65, 86, 11, 14, 12];
+        let sorted: Vec<isize> = vec![3, 11, 11, 12, 14, 32, 65, 86, 546, 6546];
         assert_eq!(quicksort(&mut arr, 0, 9), sorted);
     }
 
     #[test]
     fn quicksort_slice_of_5() {
-        let mut arr: Vec<usize> = vec![3, 546, 32, 11, 6546, 65, 86, 11, 14, 1];
-        let sorted: Vec<usize> = vec![3, 546, 11, 32, 65, 86, 6546, 11, 14, 1];
+        let mut arr: Vec<isize> = vec![3, 546, 32, 11, 6546, 65, 86, 11, 14, 1];
+        let sorted: Vec<isize> = vec![3, 546, 11, 32, 65, 86, 6546, 11, 14, 1];
         assert_eq!(quicksort(&mut arr, 2, 6), sorted);
     }
 
     #[test]
     fn quicksort_one() {
-        let mut arr: Vec<usize> = vec![3, 546, 32, 11, 6546, 65, 86, 11, 14, 1];
+        let mut arr: Vec<isize> = vec![3, 546, 32, 11, 6546, 65, 86, 11, 14, 1];
         assert_eq!(quicksort(&mut arr, 3, 3), arr);
     }
 }
